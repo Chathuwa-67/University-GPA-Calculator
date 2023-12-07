97514a2d1a380b33c89c7ae79fef566dd91a0ab7
Gihan Chathuranga Gunathilaka <141658614+Chathuwa-67@users.noreply.github.com>
2023-12-07 22:19:36 +0530

    Update UniversityGPACalculator.java
    Divided into classes.
    used OOP principles

--- UniversityGPACalculator.java
+++ UniversityGPACalculator.java
@@ -1,187 +1,199 @@
 import java.util.Scanner;
 
-class Subject {
-    private String subjectName;
-    private int subjectCode;
-
-    public Subject(String subjectName, int subjectCode) {
-        this.subjectName = subjectName;
-        this.subjectCode = subjectCode;
-    }
-
-    public String getSubjectName() {
-        return subjectName;
-    }
-
-    public int getSubjectCode() {
-        return subjectCode;
-    }
-}
-
-class Semester {
-    private Subject[] subjects;
-
-    public Semester(Subject[] subjects) {
-        this.subjects = subjects;
-    }
-
-    public Subject[] getSubjects() {
-        return subjects;
-    }
-}
-
-class AcademicYear {
-    private Semester[] semesters;
-
-    public AcademicYear(Semester[] semesters) {
-        this.semesters = semesters;
-    }
-
-    public Semester[] getSemesters() {
-        return semesters;
-    }
-}
-
 class Student {
     private String name;
-    private String indexNumber;
-    private AcademicYear[] academicYears;
+    private int indexNumber;
 
-    public Student(String name, String indexNumber, AcademicYear[] academicYears) {
+    public Student(String name, int indexNumber) {
         this.name = name;
         this.indexNumber = indexNumber;
-        this.academicYears = academicYears;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getIndexNumber() {
+    public int getIndexNumber() {
         return indexNumber;
-    }
-
-    public AcademicYear[] getAcademicYears() {
-        return academicYears;
     }
 }
 
-public class UniversityGPACalculatorOOP {
+class AcademicYear {
+    private int year;
 
+    public AcademicYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+class Semester {
+    private int semester;
+
+    public Semester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+}
+
+class Subject {
+    private static final String[][] subjectNames = {
+            {"Essentials of ICT", "Mathematics for Technology", "Fundamentals of Computer Programming", "Fundamentals of Web Technologies ", "Principles of Management ", "English Language I "},
+            {"SubjectA2", "SubjectB2", "SubjectC2", "SubjectD2", "SubjectE2", "SubjectF2"},
+            {"SubjectA3", "SubjectB3", "SubjectC3", "SubjectD3", "SubjectE3", "SubjectF3"},
+            {"SubjectA4", "SubjectB4", "SubjectC4", "SubjectD4", "SubjectE4", "SubjectF4"}
+    };
+
+    private static final int[][] subjectCodes = {
+            {1114, 1123, 1134, 1142, 1152, 1110},
+            {201, 202, 203, 204, 205, 206},
+            {301, 302, 303, 304, 305, 306},
+            {401, 402, 403, 404, 405, 406}
+    };
+
+    public String getSubjectName(int year, int semester, int subjectIndex) {
+        return subjectNames[year][semester * 2 + subjectIndex];
+    }
+
+    public int getSubjectCode(int year, int semester, int subjectIndex) {
+        return subjectCodes[year][semester * 2 + subjectIndex];
+    }
+
+    public int getSubjectCredit(int subjectCode) {
+        return subjectCode % 10;
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("########################################################");
+        System.out.println();
+        System.out.println("--------Welcome to the University of Vavuniya---------");
+        System.out.println();
+        System.out.println(" ----------Faculty of Technological Studies----------");
+        System.out.println();
+        System.out.println(" Department of Information and Communication Technology ");
+        System.out.println();
+        System.out.println("\t\tGPA Calculator ");
+        System.out.println();
+        System.out.println("########################################################");
 
-        System.out.print("Enter student name: ");
+        // Get student information
+        System.out.print("\nEnter student name: ");
         String studentName = scanner.nextLine();
+        System.out.print("\nEnter student index number: ");
+        int indexNumber = scanner.nextInt();
 
-        System.out.print("Enter student index number: ");
-        String indexNumber = scanner.nextLine();
+        Student student = new Student(studentName, indexNumber);
 
-        AcademicYear[] academicYears = createAcademicYears();
+        // Get academic year
+        System.out.print("\nSelect academic year (1-4): ");
+        int selectedYear = scanner.nextInt();
+        AcademicYear academicYear = new AcademicYear(selectedYear - 1);
 
-        Student student = new Student(studentName, indexNumber, academicYears);
+        // Get semester
+        System.out.print("\nSelect semester (1-2): ");
+        int selectedSemester = scanner.nextInt();
+        Semester semester = new Semester(selectedSemester - 1);
 
-        double finalGPA = calculateFinalGPA(student);
+        // Display subjects
+        Subject subjects = new Subject();
+        for (int i = 0; i < 6; i++) {
+            String subjectName = subjects.getSubjectName(selectedYear - 1, selectedSemester - 1, i);
+            int subjectCode = subjects.getSubjectCode(selectedYear - 1, selectedSemester - 1, i);
+            int subjectCredit = subjects.getSubjectCredit(subjectCode);
 
-        System.out.println("\nStudent Name: " + student.getName());
-        System.out.println("Index Number: " + student.getIndexNumber());
-
-        for (int i = 0; i < academicYears.length; i++) {
-            System.out.println("\nAcademic Year " + (i + 1) + ":");
-
-            AcademicYear academicYear = academicYears[i];
-
-            for (int j = 0; j < academicYear.getSemesters().length; j++) {
-                System.out.println("\nSemester " + (j + 1) + ":");
-                Semester semester = academicYear.getSemesters()[j];
-                double semesterGPA = calculateSemesterGPA(semester);
-                System.out.println("Semester GPA: " + semesterGPA);
-            }
+           // System.out.println("Enter result for " + subjectName + " (Credit: " + subjectCredit + "): ");
+            // Get user input for results (grades, etc.)
+            // You can implement this part based on your requirements
         }
 
-        System.out.println("\nFinal GPA: " + finalGPA);
+        // Calculate GPA and display results
+        double totalCredits = 0;
+        double totalGradePoints = 0;
+
+        for (int i = 0; i < 6; i++) {
+            String subjectName = subjects.getSubjectName(selectedYear - 1, selectedSemester - 1, i);
+            int subjectCode = subjects.getSubjectCode(selectedYear - 1, selectedSemester - 1, i);
+            int subjectCredit = subjects.getSubjectCredit(subjectCode);
+
+            System.out.print("\nEnter grade for " + subjectName + ": ");
+            String grade = scanner.next();
+
+            double gradePoint = calculateGradePoint(grade);
+            totalCredits += subjectCredit;
+            totalGradePoints += gradePoint * subjectCredit;
+        }
+
+        // Calculate GPA
+        double gpa = totalGradePoints / totalCredits;
+
+        // Display results
+        
+        System.out.println("########################################################");
+        System.out.println();
+        System.out.println("\nStudent Information:");
+        System.out.println("\nName: " + student.getName());
+        System.out.println("\nIndex Number: " + student.getIndexNumber());
+        System.out.println("\nAcademic Year: " + academicYear.getYear()+1);
+        System.out.println("\nSemester: " + semester.getSemester()+1);
+        
+
+        // System.out.println("\nSubject-wise GPA:");
+
+        // for (int i = 0; i < 6; i++) {
+        //     String subjectName = subjects.getSubjectName(selectedYear - 1, selectedSemester - 1, i);
+        //     int subjectCode = subjects.getSubjectCode(selectedYear - 1, selectedSemester - 1, i);
+        //     int subjectCredit = subjects.getSubjectCredit(subjectCode);
+
+        //     System.out.println(subjectName + ": " + calculateGradePoint(scanner.next()));
+        // }
+
+        System.out.println("\nTotal GPA: " + gpa);
+        System.out.println("########################################################");
+        System.out.println();
 
         scanner.close();
     }
 
-    private static AcademicYear[] createAcademicYears() {
-        // Create subjects
-        Subject[] subjects = new Subject[8];
-        subjects[0] = new Subject("Subject1", 101);
-        subjects[1] = new Subject("Subject2", 102);
-        subjects[2] = new Subject("Subject3", 103);
-        subjects[3] = new Subject("Subject4", 104);
-        subjects[4] = new Subject("Subject5", 105);
-        subjects[5] = new Subject("Subject6", 106);
-        subjects[6] = new Subject("Subject7", 107);
-        subjects[7] = new Subject("Subject8", 108);
-
-        // Create semesters
-        Semester[] semesters = new Semester[8];
-        semesters[0] = new Semester(new Subject[]{subjects[0], subjects[1]});
-        semesters[1] = new Semester(new Subject[]{subjects[2], subjects[3]});
-        semesters[2] = new Semester(new Subject[]{subjects[4], subjects[5]});
-        semesters[3] = new Semester(new Subject[]{subjects[6], subjects[7]});
-        semesters[4] = new Semester(new Subject[]{subjects[0], subjects[2]});
-        semesters[5] = new Semester(new Subject[]{subjects[4], subjects[6]});
-        semesters[6] = new Semester(new Subject[]{subjects[1], subjects[3]});
-        semesters[7] = new Semester(new Subject[]{subjects[5], subjects[7]});
-
-        // Create academic years
-        AcademicYear[] academicYears = new AcademicYear[4];
-        academicYears[0] = new AcademicYear(new Semester[]{semesters[0], semesters[1]});
-        academicYears[1] = new AcademicYear(new Semester[]{semesters[2], semesters[3]});
-        academicYears[2] = new AcademicYear(new Semester[]{semesters[4], semesters[5]});
-        academicYears[3] = new AcademicYear(new Semester[]{semesters[6], semesters[7]});
-
-        return academicYears;
-    }
-
-    private static double calculateSemesterGPA(Semester semester) {
-        double totalCredits = 0;
-        double totalGradePoints = 0;
-
-        for (Subject subject : semester.getSubjects()) {
-            int subjectCode = subject.getSubjectCode();
-            int credit = subjectCode % 10; // Extract last digit for credit
-            totalCredits += credit;
-
-            // For simplicity, assuming grade as 'A' for all subjects
-            double gradePoints = getGradePoints('A');
-            totalGradePoints += gradePoints * credit;
-        }
-
-        return totalGradePoints / totalCredits;
-    }
-
-    private static double calculateFinalGPA(Student student) {
-        double totalCredits = 0;
-        double totalGradePoints = 0;
-
-        for (AcademicYear academicYear : student.getAcademicYears()) {
-            for (Semester semester : academicYear.getSemesters()) {
-                for (Subject subject : semester.getSubjects()) {
-                    int subjectCode = subject.getSubjectCode();
-                    int credit = subjectCode % 10; // Extract last digit for credit
-                    totalCredits += credit;
-
-                    // For simplicity, assuming grade as 'A' for all subjects
-                    double gradePoints = getGradePoints('A');
-                    totalGradePoints += gradePoints * credit;
-                }
-            }
-        }
-
-        return totalGradePoints / totalCredits;
-    }
-
-    private static double getGradePoints(char grade) {
-        // Implement your grading logic here
+    private static double calculateGradePoint(String grade) {
         switch (grade) {
-            case 'A':
+            case "A+":
                 return 4.0;
-            // Add more cases for other grades as needed
+            case "A":
+                return 4.0;
+            case "A-":
+                return 3.7;
+             case "B+":
+                return 3.3;
+            case "B":
+                return 3.0;
+            case "B-":
+                return 2.7;
+            case "C+":
+                return 2.3;
+            case "C":
+                return 2.0;
+            case "C-":
+                return 1.7;
+            case "D+":
+                return 1.3;
+            case "D":
+                return 1.0;
+            case "D-":
+                return 0.7;
+            case "E":
+                return 0.0;
+
             default:
+                System.out.println("Invalid grade. Assuming grade point as 0.0.");
                 return 0.0;
         }
     }
